f3888976ed84c7e73c585d4ceb8bbc6c56a3ac44
devin <64448570+rattrayd11@users.noreply.github.com>
2023-06-28 13:51:44 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,41 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import {
+  ThirdwebProvider,
+  coinbaseWallet,
+  magicLink,
+  metamaskWallet,
+  paperWallet,
+  safeWallet,
+  walletConnect,
+} from "@thirdweb-dev/react";
+// import { FantomTestnet } from "@thirdweb-dev/chains";
 import "../styles/globals.css";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+
+// const emailWallet = magicLink({
+//   apiKey: process.env.NEXT_PUBLIC_MAGIC_API as string,
+// });
+
+// const emailWallet2 = paperWallet({
+//   clientId: "",
+// });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={activeChain}>
+    <ThirdwebProvider
+      activeChain={"goerli"}
+       supportedWallets={[
+        metamaskWallet(),
+        // coinbaseWallet(),
+        // safeWallet(),
+        // walletConnect(),
+        // emailWallet,
+       // emailWallet2,
+      ]}
+    >
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
